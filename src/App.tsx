01a2e264bd41fcea9ffeafc85e6aeafab7c1c500
Bayley21 <bayley1800@gmail.com>
2024-10-30 20:10:13 +0000

    Removed boilerplate, created new type, basic template started

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Item } from './types/item'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cart, setCart] = useState<Item[]>([])
+  const items = ['A','B','C','D'];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container mx-auto">
+        <div className='grid grid-cols-4 space-x-6'>
+          {items.map((v, i) => <div className='text-center rounded-lg border p-4'>
+            {v}
+          </div>)}
+        </div>
+        <hr className='my-16'/>
+          <div className='flex justify-center gap-4 items-center'>
+            <h3 className='text-lg font-bold'>Cart</h3>
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
